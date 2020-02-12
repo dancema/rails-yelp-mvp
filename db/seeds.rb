bd85a097d6d6392830c9ed4c89b2c01245fa2af3
DanCema <dan.cemachovic@edhec.com>
2020-02-12 12:37:04 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+restaurants = [
+{
+  name: "La Famille",
+  address: "Port de lille",
+  phone_number: "+6282983",
+  category: "chinese"
+},
+{
+  name: "Tour d'Argent",
+  address: "Quai de paris",
+  phone_number: "+62992983",
+  category: "french"
+},
+{
+  name: "Sushi shop",
+  address: "Opera",
+  phone_number: "+628280923",
+  category: "japanese"
+},
+{
+  name: "Big Mamma",
+  address: "Rue de de la pizza",
+  phone_number: "+628290823",
+  category: "italian"
+},
+{
+  name: "La gaufre",
+  address: "Rue de Bruxelles",
+  phone_number: "+62890920823",
+  category: "belgian"
+}
+]
+
+Restaurant.create(restaurants)
+puts "completed"
