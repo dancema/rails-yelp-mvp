f14fa9feff6d64963c881cc7f0712b74448faaf9
DanCema <dan.cemachovic@edhec.com>
2020-02-12 16:08:46 +0100

    done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,12 @@
 class ReviewsController < ApplicationController
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
-
   def create
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = @review.restaurant
     if @review.save
       redirect_to restaurant_path(@review.restaurant)
     else
-      render :new
+      render 'restaurants/show'
     end
   end
 
