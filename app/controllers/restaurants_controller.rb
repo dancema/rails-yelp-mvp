8705b08fbc964009baeb452f357d2ef4bab2c2f2
DanCema <dan.cemachovic@edhec.com>
2020-02-12 13:02:23 +0100

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,23 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    Restaurant.create(restaurant_params)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :phone_number, :address)
   end
 end
